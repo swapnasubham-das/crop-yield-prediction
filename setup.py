d0ba38f6978bef5a5921a076224772e72976bfb6
Swapnasubham Das <swapnasubham.das@gmail.com>
2025-10-20 21:29:41 +0530

    Initial commit: Project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML system for predicting crop yields using environmental and agricultural data',
+    author='Swapnasubham Das',
+    license='',
+)
